d31c81f7a3c5deab247eb47dd3637273b9c4af64
Aynur <aynur182@gmail.com>
2016-04-10 22:18:54 +0300

    lk lk lk ..... *gona murder someone*

---
+++ app/src/main/java/com/blink/treecommunicationproject/Web/Links.java
@@ -0,0 +1,35 @@
+//package com.blink.treecommunicationproject.Web;
+//
+///**
+// * Created by Ahmad on 10/7/2015.
+// */
+//public class Links {
+//    public static String HOST = "http://ahmadhammoud.com/Traffic/";
+//
+//    public static String ACTIONS = HOST + "actions/";
+//    public static String INSERTUSER = ACTIONS + "InsertUser.php";
+//    public static String GETUSER = ACTIONS + "GetUser.php";
+//    public static String GETBROADCASTS = ACTIONS + "GetBroadcasts.php";
+//    public static String INSERTBROADCAST = ACTIONS + "InsertBroadcast.php";
+//    public static String INSERTIMAGEBROADCAST = ACTIONS + "InsertImageBroadcast.php";
+//    public static String INSERTPROFILEPICTURE = ACTIONS + "InsertProfilePicture.php";
+//    public static String UPDATEUSER = ACTIONS + "UpdateUser.php";
+//    public static String INSERTUSERSTATUS = ACTIONS + "InsertUserStatus.php";
+//    public static String GETTRAFFICLIGHTSSTATUS = ACTIONS + "GetTrafficLightsStatus.php";
+//    public static String GETTRAFFICLIGHTSSTATUSWITHBROADCASTS = ACTIONS + "GetTrafficLightsStatusWithBroadcasts.php";
+//    public static String GETTRAFFICLIGHTHISTORY = ACTIONS + "GetTrafficLightHistory.php";
+//    public static String GETTRAFFIC = ACTIONS + "GetTraffic.php";
+//    public static String GETPATHWITHREALTIMEINFO = ACTIONS + "GetPathWithRealTimeInfo.php";
+//    public static String GETPATH = ACTIONS + "GetPath.php";
+//    public static String SENDMESSAGE = ACTIONS + "SendMessage.php";
+//    public static String GETMESSAGES = ACTIONS + "GetMessages.php";
+//    public static String GETLASTMESSAGE = ACTIONS + "GetLastMessage.php";
+//
+//    public static String PROFILEIMAGES = HOST + "images/profiles/";
+//    public static String BROADCASTIMAGES = HOST + "images/broadcasts/";
+//    public static String TRAFFICLIGHTSIMAGES = HOST + "images/lights/";
+//
+//    public static String SNAPTOROAD(String latLngs){
+//        return "https://roads.googleapis.com/v1/snapToRoads?path=" + latLngs + "&key=AIzaSyBJ_Ta4ZhbE4nw7gSEOdOofBwEtuLiJ7II";
+//    }
+//}
