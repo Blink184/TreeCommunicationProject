d2e9ea736566ea35f3eba95d642e8e83aaf6db72
Aynur <aynur182@gmail.com>
2016-04-23 12:06:50 +0300

    tfadal shufa

--- app/src/main/java/com/blink/treecommunicationproject/Activities/Fragments/AssignNewTaskFragment.java
+++ app/src/main/java/com/blink/treecommunicationproject/Activities/Fragments/AssignNewTaskFragment.java
@@ -1,12 +1,67 @@
 package com.blink.treecommunicationproject.Activities.Fragments;
 
 import android.app.Fragment;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Adapter;
+import android.widget.AutoCompleteTextView;
+import android.widget.BaseAdapter;
+import android.widget.ImageButton;
+import android.widget.Toast;
 
+import com.blink.treecommunicationproject.Objects.Employee;
 import com.blink.treecommunicationproject.R;
+import com.blink.treecommunicationproject.Services.Global;
+
+import java.util.ArrayList;
+import java.util.Date;
 
 
 public class AssignNewTaskFragment extends Fragment {
 
-    public AssignNewTaskFragment() {
+    private View rootView;
+    private ArrayList<Employee> toEmployees = new ArrayList<>();
+    private ImageButton send;
+    private AutoCompleteTextView actvToEmployee;
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        rootView = inflater.inflate(R.layout.fragment_task, container, false);
+
+        toEmployees.add(new Employee(0, "Susan", "J. Mordigan", "03555666", Employee.EmployeeType.Advisor));
+        toEmployees.add(new Employee(0, "Jack", "Black", "78959565", Employee.EmployeeType.Responsible));
+        toEmployees.add(new Employee(0, "Elie", "Nassar", "70112547", Employee.EmployeeType.Responsible));
+        toEmployees.add(new Employee(0, "John", "Blue", "81235689", Employee.EmployeeType.Director));
+
+        return rootView;
     }
+
+    public void initialize() {
+        send = (ImageButton) rootView.findViewById(R.id.btnSendTask);
+        actvToEmployee = (AutoCompleteTextView) rootView.findViewById(R.id.actvToEmployee);
+
+
+
+        send.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getActivity(), "Your task has been sent", Toast.LENGTH_SHORT).show();
+
+                Fragment fragment = new TaskFragment();
+                FragmentManager fragmentManager = getActivity().getFragmentManager();
+                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+                fragmentTransaction.replace(R.id.container, fragment);
+                fragmentTransaction.addToBackStack(null);
+                fragmentTransaction.commit();
+            }
+        });
+
+    }
+
 }
