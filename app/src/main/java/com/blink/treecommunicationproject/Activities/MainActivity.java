a75b0af71faae53d00c913f9c6cd9630127becdb
Aynur <aynur182@gmail.com>
2016-04-29 18:23:33 +0300

    pushing changes

--- app/src/main/java/com/blink/treecommunicationproject/Activities/MainActivity.java
+++ app/src/main/java/com/blink/treecommunicationproject/Activities/MainActivity.java
@@ -14,6 +14,7 @@ import android.widget.Toast;
 
 import com.blink.treecommunicationproject.Activities.Fragments.AllEmployeesFragment;
 import com.blink.treecommunicationproject.Activities.Fragments.HomeFragment;
+import com.blink.treecommunicationproject.Activities.Fragments.SendBroadcastFragment;
 import com.blink.treecommunicationproject.Activities.Fragments.SendMessageFragment;
 import com.blink.treecommunicationproject.Activities.Fragments.TaskFragment;
 import com.blink.treecommunicationproject.Activities.Fragments.ToDoTaskFragment;
@@ -57,6 +58,9 @@ public class MainActivity extends AppCompatActivity implements NavigationDrawerC
                     fragment = new SendMessageFragment();
                     break;
                 case 4:
+                    fragment = new SendBroadcastFragment();
+                    break;
+                case 5:
                     fragment = new AllEmployeesFragment();
                     break;
                 default:
