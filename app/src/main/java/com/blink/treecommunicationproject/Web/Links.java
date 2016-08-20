6abf1f23c7679051638eb6fb1340aaff2cd82ed9
ahmmmoud <ahmmmoud@gmail.com>
2016-08-21 01:32:51 +0300

    design fix + ability to change IP on login

--- app/src/main/java/com/blink/treecommunicationproject/Web/Links.java
+++ app/src/main/java/com/blink/treecommunicationproject/Web/Links.java
@@ -6,7 +6,8 @@ package com.blink.treecommunicationproject.Web;
 public class Links {
     /*public static String HOST = "http://192.168.1.6/TreeCommunicationProjectWeb/";*/
 
-    public static String HOST = "http://10.0.2.2/TreeCommunicationProjectWeb/";
+    public static String IP = "10.0.2.2:80";
+    public static String HOST = "http://" + IP + "/TreeCommunicationProjectWeb/";
     //http://localhost:63343/TreeCommunicationProjectWeb/database/api/validateUser.php
 
     public static String ACTIONS = HOST + "database/api/";
@@ -15,4 +16,14 @@ public class Links {
     public static String GETTASKS = ACTIONS + "getTasks.php";
     public static String ACCEPTTASK = ACTIONS + "acceptTask.php";
     public static String FINISHTASK = ACTIONS + "finishTask.php";
+
+    public static void ipChanged(){
+        HOST = "http://" + IP + "/TreeCommunicationProjectWeb/";
+        ACTIONS = HOST + "database/api/";
+        VALIDATEUSER = ACTIONS + "validateUser.php";
+        GETTREE = ACTIONS + "getUserRoleTree.php";
+        GETTASKS = ACTIONS + "getTasks.php";
+        ACCEPTTASK = ACTIONS + "acceptTask.php";
+        FINISHTASK = ACTIONS + "finishTask.php";
+    }
 }
