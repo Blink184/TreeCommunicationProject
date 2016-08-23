40557fdbe392517bc784e488ea10b4ba97d205ec
ahmmmoud <ahmmmoud@gmail.com>
2016-08-23 18:29:55 +0300

    images of employees in the tree

--- app/src/main/java/com/blink/treecommunicationproject/Web/Links.java
+++ app/src/main/java/com/blink/treecommunicationproject/Web/Links.java
@@ -8,6 +8,7 @@ public class Links {
 
     public static String IP = "10.0.2.2:80";
     public static String HOST = "http://" + IP + "/TreeCommunicationProjectWeb/";
+    public static String PROFILEPICTURESFOLDER = HOST + "resources/images/employee/users/";
     //http://localhost:63343/TreeCommunicationProjectWeb/database/api/validateUser.php
 
     public static String ACTIONS = HOST + "database/api/";
@@ -22,6 +23,7 @@ public class Links {
     public static void ipChanged(){
         HOST = "http://" + IP + "/TreeCommunicationProjectWeb/";
         ACTIONS = HOST + "database/api/";
+        PROFILEPICTURESFOLDER = HOST + "resources/images/employee/users/";
         VALIDATEUSER = ACTIONS + "validateUser.php";
         GETTREE = ACTIONS + "getUserRoleTree.php";
         GETTASKS = ACTIONS + "getTasks.php";
